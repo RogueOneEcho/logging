bb56db1212bbb278e535c3d93c86a71229adc45e
RogueOneEcho <Um9ndWVPbmVFY2hv@pm.me>
2024-11-03 15:31:32 +0000

    style: allow unwrap for tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,11 @@ pub use verbosity::*;
 mod colors;
 mod error;
 #[cfg(test)]
+#[allow(clippy::unwrap_used)]
 mod error_tests;
 mod logger;
 #[cfg(test)]
+#[allow(clippy::unwrap_used)]
 mod logger_tests;
 mod time_format;
 mod verbosity;
